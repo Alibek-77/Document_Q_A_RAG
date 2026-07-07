2461f1d5923754423b131effc2335cebd9e261e4
Alibek-77 <mlalalsksks@gmail.com>
2026-07-07 19:39:47 +0500

    Delete python.py

--- python.py
+++
@@ -1,7 +0,0 @@
-def tokenizing_document(doc:list[str]):
-    tokens=[]
-    for d in doc:
-        for token in d.lower().split():
-            tokens.append(token)
-    return tokens
-print(tokenizing_document(["ALibek krash sila","lkfs fdksf fdsk lf"]))
